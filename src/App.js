33225d90065770032c14e61a76e821b867fbf1d8
Chun-Lin <wulin40063@gmail.com>
2019-08-23 11:14:40 +0800

    feat: 🎸 add a custom checkbox button by using framer api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Frame, useCycle } from 'framer'
+import './App.css'
 
 function App() {
+  const [left, setLeft] = useCycle(0, 50)
+  const tapHandler = () => {
+    setLeft()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Frame width="100%" height="100%" background="transparent">
+      <Frame
+        width={100}
+        height={50}
+        background="#ddd"
+        borderRadius={25}
+        center
+        onTap={tapHandler}
+      >
+        <Frame
+          width={50}
+          height={50}
+          background="skyblue"
+          borderRadius={25}
+          animate={{ left: left }}
+          transition={{ duration: 0.5 }}
+        />
+      </Frame>
+    </Frame>
+  )
 }
 
-export default App;
+export default App
